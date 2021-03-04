5596c22ac51487c6bd5bb3eb26a884d25240662c
adityapatel-sudo <adityapatel1204@gmail.com>
2021-03-03 23:10:11 -0800

    added all html links and css files for future use

--- public/js/nav-footer.js
+++ public/js/nav-footer.js
@@ -1,9 +1,9 @@
 const nav = "<div id=\"nav\" class=\"row text-right justify-content-end\">\n" +
-    "    <a href=\"#\" class=\"navLink\">HOME</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">ABOUT</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">SENSORS</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">LAUNCH</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">CONTRIBUTORS</a>\n" +
+    "    <a href=\index.html class=\"navLink\">HOME</a>\n" +
+    "    <a href=\about.html class=\"navLink\">ABOUT</a>\n" +
+    "    <a href=\sensors.html class=\"navLink\">SENSORS</a>\n" +
+    "    <a href=\launch.html class=\"navLink\">LAUNCH</a>\n" +
+    "    <a href=\contributors.html class=\"navLink\">CONTRIBUTORS</a>\n" +
     "</div>"
 $( document ).ready(() => {
     document.getElementById("include-nav").innerHTML = nav
