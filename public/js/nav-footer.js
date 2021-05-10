390a665d8b2e100373d1d2d203c249736d8cb93c
Varun Chitturi <varunchitturi@icloud.com>
2021-05-10 14:39:24 -0700

    Create launch page

--- public/js/nav-footer.js
+++ public/js/nav-footer.js
@@ -2,7 +2,7 @@ const nav = "<div id=\"nav\" class=\"row text-right justify-content-end\">\n" +
     "    <a href=\"index.html\" class=\"navLink\">HOME</a>\n" +
     "    <a href=\"about.html\" class=\"navLink\">ABOUT</a>\n" +
     "    <a href=\"sensors.html\" class=\"navLink\">SENSORS</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">LAUNCH</a>\n" +
+    "    <a href=\"launch.html\" class=\"navLink\">LAUNCH</a>\n" +
     "    <a href=\"#\" class=\"navLink\">CONTRIBUTORS</a>\n" +
     "</div>"
 $( document ).ready(() => {
