fb4d95ce1a3e4eedac720f72afbaa591784e9f27
Varun Chitturi <varunchitturi@icloud.com>
2021-05-10 13:36:22 -0700

    Create about page

--- public/js/nav-footer.js
+++ public/js/nav-footer.js
@@ -1,6 +1,6 @@
 const nav = "<div id=\"nav\" class=\"row text-right justify-content-end\">\n" +
     "    <a href=\"index.html\" class=\"navLink\">HOME</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">ABOUT</a>\n" +
+    "    <a href=\"about.html\" class=\"navLink\">ABOUT</a>\n" +
     "    <a href=\"sensors.html\" class=\"navLink\">SENSORS</a>\n" +
     "    <a href=\"#\" class=\"navLink\">LAUNCH</a>\n" +
     "    <a href=\"#\" class=\"navLink\">CONTRIBUTORS</a>\n" +
