b9d4c6cb09ec1d324d8ffa5a0261b50427bf0542
Varun Chitturi <varunchitturi@icloud.com>
2021-05-10 15:37:35 -0700

    Add contributors

--- public/js/nav-footer.js
+++ public/js/nav-footer.js
@@ -3,7 +3,7 @@ const nav = "<div id=\"nav\" class=\"row text-right justify-content-end\">\n" +
     "    <a href=\"about.html\" class=\"navLink\">ABOUT</a>\n" +
     "    <a href=\"sensors.html\" class=\"navLink\">SENSORS</a>\n" +
     "    <a href=\"launch.html\" class=\"navLink\">LAUNCH</a>\n" +
-    "    <a href=\"#\" class=\"navLink\">CONTRIBUTORS</a>\n" +
+    "    <a href=\"contributors.html\" class=\"navLink\">CONTRIBUTORS</a>\n" +
     "</div>"
 $( document ).ready(() => {
     document.getElementById("include-nav").innerHTML = nav
