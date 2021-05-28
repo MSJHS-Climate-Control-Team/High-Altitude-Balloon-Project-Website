a634641d962f6fedfd2682e1d3b2394653c3eed7
crystalrchen <crystalrchen@users.noreply.github.com>
2021-05-27 22:47:55 -0700

    add sensor descriptions

--- public/js/full-page-scroll.js
+++ public/js/full-page-scroll.js
@@ -115,7 +115,7 @@
 			window.addEventListener("hashchange", this.hashChange, false);
 
 			/**
-			 * Enable scroll if decive don't have touch support
+			 * Enable scroll if device don't have touch support
 			 */
 			if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 				if(!('ontouchstart' in window)){
